971fa3405d94adfe89476ac00501fa3f7ec543cb
Rafael Rodrigues Gomes <rafarodrigs3@gmail.com>
2024-10-15 21:21:27 -0300

    Conf. Gitignore e pasta projeto

---
+++ projeto_pc_solidario/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for projeto_pc_solidario project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_pc_solidario.settings')
+
+application = get_wsgi_application()
