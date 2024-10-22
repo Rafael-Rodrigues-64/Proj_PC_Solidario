0149cd04e2bc068c4f635d2d20b1c20efec9a7f0
Rafael Rodrigues Gomes <rafarodrigs3@gmail.com>
2024-10-22 17:01:18 -0300

    Template_Home

--- app_home/views.py
+++ app_home/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'app_home/pages/index.html')
 
+def app_home(request):
+    return render(request, 'app_home/pages/index.html')
+
